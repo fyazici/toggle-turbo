cc3d1130960c2840d4e4d4fce63a5ad07b5a63a9
Fatih Yazıcı <fatihyazici67@gmail.com>
2022-08-23 04:46:36 +0300

    windows build script fix

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @echo off
 
 echo Add MinGW to path
-set PATH=%PATH%;C:\Program Files\CodeBlocks\MinGW\bin
+rem set PATH=%PATH%;C:\Dev-Cpp\bin
 
 echo Build main object
-gcc -c -o main.o main.c
+g++ -Wall -Wextra -c -o main.o -Os -ggdb src/main.cpp
 
 echo Build resource object
 windres -o main.rc.o main.rc
 
 echo Link objects
-gcc -o toggle-turbo.exe main.o main.rc.o
+g++ -o toggle-turbo.exe main.o main.rc.o
