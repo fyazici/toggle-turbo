e249246556c2aa13fc7757b8344d9c9cde595311
Fatih Yazıcı <fatihyazici67@gmail.com>
2022-08-24 03:38:38 +0300

    code structure updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,56 @@
 #include <cstdio>
+#include <cstdlib>
+#include <memory>
 
-extern "C" {
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shellapi.h>
-};
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 class TurboController
 {
 public:
-    bool set_state(bool state)
+    TurboController(): state_{false} {} // TODO: retrieve initial state
+    
+    auto set_state(bool state) noexcept -> bool
+    {
+        if (!apply_power_scheme(state)) return false;
+        state_ = state;
+        return true;
+    }
+
+    auto get_state() const noexcept -> bool {
+        return state_;
+    }
+
+    auto toggle_state() noexcept -> bool
+    {
+        return set_state(!get_state());
+    }
+
+private:
+    auto find_trim_guid(char * first, const size_t length) const noexcept -> const char *
+    {
+        const char * const end = first + length;
+        
+        while ((*first != ':') && (first < end))
+            ++first;
+        ++first;
+        ++first;
+        if (first >= end) return nullptr;
+
+        char *tmp = first;
+        while ((*tmp != ' ') && (tmp < end))
+            ++tmp;
+        if (first >= end) return nullptr;
+        
+        *tmp = 0;
+
+        return first;
+    }
+
+    auto apply_power_scheme(const bool state) const noexcept -> bool
     {
         const size_t PIPE_BUFFER_LENGTH = 128, CMD_BUFFER_LENGTH = 512;
         const char *CMD_AC_ON_F = "powercfg.exe /SETACVALUEINDEX %s 54533251-82be-4824-96c1-47b60b740d00 be337238-0d82-4146-a960-4f3749d470c7 003";
@@ -22,86 +61,141 @@ public:
 
         char proc_out[PIPE_BUFFER_LENGTH];
         FILE *fd = ::popen("powercfg.exe /GETACTIVESCHEME", "r");
-        ::fgets(proc_out, PIPE_BUFFER_LENGTH, fd);
+        auto r = ::fgets(proc_out, PIPE_BUFFER_LENGTH, fd);
         ::pclose(fd);
 
+        if (r != proc_out) return false;
+
         auto guid_str = find_trim_guid(proc_out, PIPE_BUFFER_LENGTH);
+        
+        if (!guid_str) return false;
 
         char system_cmd[CMD_BUFFER_LENGTH];
         if (state)
         {
-            ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_AC_ON_F, guid_str);
+            auto r = ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_AC_ON_F, guid_str);
+            if ((r < 0) || (r == CMD_BUFFER_LENGTH)) return false;
+            
             ::system(system_cmd);
-            ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_DC_ON_F, guid_str);
+
+            r = ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_DC_ON_F, guid_str);
+            if ((r < 0) || (r == CMD_BUFFER_LENGTH)) return false;
+            
             ::system(system_cmd);
         }
         else
         {
-            ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_AC_OFF_F, guid_str);
+            auto r = ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_AC_OFF_F, guid_str);
+            if ((r < 0) || (r == CMD_BUFFER_LENGTH)) return false;
+            
             ::system(system_cmd);
-            ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_DC_OFF_F, guid_str);
+
+            r = ::snprintf(system_cmd, CMD_BUFFER_LENGTH, CMD_DC_OFF_F, guid_str);
+            if ((r < 0) || (r == CMD_BUFFER_LENGTH)) return false;
+            
             ::system(system_cmd);
         }
 
         ::system(CMD_CURRENT_SCHEME);
-        state_ = state;
         return true;
     }
 
-    auto get_state() -> bool {
-        return state_;
-    }
-
-    void toggle_state()
-    {
-        set_state(!state_);
-    }
-
-    TurboController() : state_{false} {} // TODO: retrieve initial state
-
-private:
     bool state_;
-
-    const char *find_trim_guid(char * first, size_t length)
-    {
-        const char * const end = first + length;
-        char *tmp;
-        while ((*first != ':') && (*first != 0) && (first != end))
-            ++first;
-        ++first;
-        ++first;
-        tmp = first;
-        while ((*tmp != ' ') && (*tmp != 0) && (tmp != end))
-            ++tmp;
-        *tmp = 0;
-        return first;
-    }
 };
 
-class App
+class TurboNotifyIcon
 {
-private:
-    TurboController ctl_;
-
 public:
     constexpr static const size_t ID_TRAY_APP_ICON = 5000;
     constexpr static const size_t WM_TRAYICON = (WM_USER + 1);
 
-    UINT WM_TASKBARCREATED = 0;
-
-    HINSTANCE hinstance_;
-    HWND hwnd_;
-    NOTIFYICONDATA notify_icon_data_;
-
-    App(HINSTANCE hInstance) : hinstance_{hInstance} 
+    TurboNotifyIcon(HINSTANCE hinstance, HWND hwnd, bool state)
+        : hinstance_{hinstance}, hwnd_{hwnd}
     {
-        ctl_.set_state(true);
+        ::memset(&notify_icon_data_, 0, sizeof(NOTIFYICONDATA));
+        notify_icon_data_.cbSize = sizeof(NOTIFYICONDATA);
+        notify_icon_data_.hWnd = hwnd_;
+        notify_icon_data_.uID = ID_TRAY_APP_ICON;
+        notify_icon_data_.uFlags = NIF_ICON | NIF_MESSAGE;
+        notify_icon_data_.uCallbackMessage = WM_TRAYICON;
+        
+        notify_icon_data_.hIcon = load_turbo_icon(state);
+
+        ::Shell_NotifyIcon(NIM_ADD, &notify_icon_data_);
+        ::DestroyIcon(notify_icon_data_.hIcon);
+        notify_icon_data_.hIcon = nullptr;
     }
 
-    void init_window()
+    auto update(bool state) noexcept -> void
+    {
+        notify_icon_data_.hIcon = load_turbo_icon(state);
+        
+        ::Shell_NotifyIcon(NIM_MODIFY, &notify_icon_data_);
+        ::DestroyIcon(notify_icon_data_.hIcon);
+        notify_icon_data_.hIcon = nullptr;
+    }
+
+    virtual ~TurboNotifyIcon()
+    {
+        ::Shell_NotifyIcon(NIM_DELETE, &notify_icon_data_);
+    }
+
+private:
+    auto load_turbo_icon(bool state) const noexcept -> HICON
+    {
+        if (state)
+            return ::LoadIcon(hinstance_, TEXT("ICO_TURBO_ON"));
+        else
+            return ::LoadIcon(hinstance_, TEXT("ICO_TURBO_OFF"));
+    }
+
+    const HINSTANCE hinstance_;
+    const HWND hwnd_;
+    NOTIFYICONDATA notify_icon_data_;
+};
+
+class App
+{
+public:
+    App(HINSTANCE hInstance)
+        : hinstance_{hInstance}, turbo_ctl_{std::make_unique<TurboController>()}
+    { 
+        turbo_ctl_->set_state(true); 
+        init_window();
+    }
+
+    LRESULT handle_wndproc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+    {
+        switch (message)
+        {
+        case TurboNotifyIcon::WM_TRAYICON:
+            if (wParam == TurboNotifyIcon::ID_TRAY_APP_ICON)
+            {
+                if (lParam == WM_LBUTTONUP)
+                {
+                    turbo_ctl_->toggle_state();
+                    notify_icon_->update(turbo_ctl_->get_state());
+                }
+                else if (lParam == WM_RBUTTONUP)
+                {
+                    PostQuitMessage(0);
+                }
+                else if (lParam == WM_MBUTTONUP)
+                {
+                    display_command_help();
+                }
+            }
+            break;
+        default:
+            return ::DefWindowProc(hwnd, message, wParam, lParam);
+        }
+        return 0;
+    }
+
+private:
+    auto init_window() noexcept -> void
     {
         TCHAR className[] = TEXT("ToggleTurboClass");
-        WM_TASKBARCREATED = RegisterWindowMessageA("TaskbarCreated");
 
         WNDCLASSEX wnd;
         ::memset(&wnd, 0, sizeof(wnd));
@@ -130,43 +224,11 @@ public:
             300, 300,
             NULL, NULL,
             hinstance_, NULL);
+
+        notify_icon_ = std::make_unique<TurboNotifyIcon>(hinstance_, hwnd_, turbo_ctl_->get_state());
     }
 
-    void init_notifyicon()
-    {
-        ::memset(&notify_icon_data_, 0, sizeof(NOTIFYICONDATA));
-        notify_icon_data_.cbSize = sizeof(NOTIFYICONDATA);
-        notify_icon_data_.hWnd = hwnd_;
-        notify_icon_data_.uID = ID_TRAY_APP_ICON;
-        notify_icon_data_.uFlags = NIF_ICON | NIF_MESSAGE;
-        notify_icon_data_.uCallbackMessage = WM_TRAYICON;
-        if (ctl_.get_state())
-        {
-            notify_icon_data_.hIcon = (HICON)::LoadImage(hinstance_, TEXT("ICO_TURBO_ON"), IMAGE_ICON, 0, 0, 0);
-        }
-        else
-        {
-            notify_icon_data_.hIcon = (HICON)::LoadImage(hinstance_, TEXT("ICO_TURBO_OFF"), IMAGE_ICON, 0, 0, 0);
-        }
-        ::Shell_NotifyIcon(NIM_ADD, &notify_icon_data_);
-    }
-
-    void update_notifyicon()
-    {
-        HICON old_icon = notify_icon_data_.hIcon;
-        if (ctl_.get_state())
-        {
-            notify_icon_data_.hIcon = (HICON)::LoadImage(hinstance_, TEXT("ICO_TURBO_ON"), IMAGE_ICON, 0, 0, 0);
-        }
-        else
-        {
-            notify_icon_data_.hIcon = (HICON)::LoadImage(hinstance_, TEXT("ICO_TURBO_OFF"), IMAGE_ICON, 0, 0, 0);
-        }
-        ::Shell_NotifyIcon(NIM_MODIFY, &notify_icon_data_);
-        DestroyIcon(old_icon);
-    }
-
-    void display_command_help()
+    auto display_command_help() const noexcept -> void
     {
         int r = ::MessageBox(hwnd_, TEXT(
             "Command to enable the TurboBoost control:\n"
@@ -190,47 +252,13 @@ public:
         }
     }
 
-    LRESULT handle_wndproc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-    {
-        if (message == WM_TASKBARCREATED)
-        {
-            ::Shell_NotifyIcon(NIM_ADD, &notify_icon_data_);
-            return 0;
-        }
-
-        switch (message)
-        {
-        case WM_TRAYICON:
-            if (wParam == ID_TRAY_APP_ICON)
-            {
-                if (lParam == WM_LBUTTONUP)
-                {
-                    ctl_.toggle_state();
-                    update_notifyicon();
-                }
-                else if (lParam == WM_RBUTTONUP)
-                {
-                    PostQuitMessage(0);
-                }
-                else if (lParam == WM_MBUTTONUP)
-                {
-                    display_command_help();
-                }
-            }
-            break;
-        }
-        return ::DefWindowProc(hwnd, message, wParam, lParam);
-    }
-
-    virtual ~App()
-    {
-        ::Shell_NotifyIcon(NIM_DELETE, &notify_icon_data_);
-    }
+    HINSTANCE hinstance_;
+    HWND hwnd_;
+    std::unique_ptr<TurboController> turbo_ctl_;
+    std::unique_ptr<TurboNotifyIcon> notify_icon_;
 };
 
-namespace {
-App *app;
-}
+std::unique_ptr<App> app;
 
 int WINAPI WinMain(
     HINSTANCE hInstance, 
@@ -243,11 +271,7 @@ int WINAPI WinMain(
     ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
     #endif
 
-    App app_(hInstance);
-    app = &app_;
-
-    app->init_window();
-    app->init_notifyicon();
+    app = std::make_unique<App>(hInstance);
 
     MSG msg;
     while (::GetMessage(&msg, NULL, 0, 0))
